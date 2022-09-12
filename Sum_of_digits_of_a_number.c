019fe9e21c60d1c8768f3aa20b640c05deebd53d
AR-30 <112623706+AR-30@users.noreply.github.com>
2022-09-12 22:55:52 +0530

    Create Sum_of_digits_of_a_number.c

---
+++ Sum_of_digits_of_a_number.c
@@ -0,0 +1,14 @@
+//CALCULATES THE SUM OF THE DIGITS OF A NUMBER.
+
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d",&n);
+    int sum=0;
+    while(n!=0){
+        sum=sum+(n%10);
+        n=n/10;
+    }
+    printf("%d",sum);
+}
