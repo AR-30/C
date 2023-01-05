e9c75c37a5da9088fe7c3b0c955a827edff68b90
AR-30 <112623706+AR-30@users.noreply.github.com>
2023-01-05 19:43:16 +0530

    Update Max_occurence_count_in_array.c

--- Max_occurence_count_in_array.c
+++ Max_occurence_count_in_array.c
@@ -9,19 +9,19 @@ int main()
     scanf("%d",&n);
     int arr[n],count_arr[n];
     printf("Enter the elements of the array :\n");
-    for(int i=1; i<=n; i++){
+    for(int i=1; i<=n; i++){       //taking the array elements
         scanf("%d",&arr[i]);
     }
-    for(int i=1; i<=n; i++){
+    for(int i=1; i<=n; i++){       //fixing the element to compare
         int count=0;
-        for(int j=1; j<=n; j++){
-            if(arr[i]==arr[j])
+        for(int j=1; j<=n; j++){   //going through the array
+            if(arr[i]==arr[j])     //checking for the number and count the occurence
                 count++;   
          }
-        count_arr[i]=count;
+        count_arr[i]=count;        //making an array out of number of counts
     }
     max=count_arr[1];
-    for(int i=1;i<=n; i++){
+    for(int i=1;i<=n; i++){        //finding the max of the count of occuremces.
         if(count_arr[i]>max)
             max=count_arr[i];
     }
